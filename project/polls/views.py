fc1256af815b01cdb6326812c8df4ab8446846d5
Taemin <jtm990506@naver.com>
2022-04-30 00:12:41 +0900

    second commit

---
+++ project/polls/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)

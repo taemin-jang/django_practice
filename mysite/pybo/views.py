2f7358c23f7c95f34f0989aced0921155218ba66
Taemin <jtm990506@gmail.com>
2022-05-04 23:24:49 +0900

    [jump to django] 2-8 Bootstrap part

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 
 
 
@@ -7,3 +8,18 @@ def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list' : question_list}
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # answer.save()
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)

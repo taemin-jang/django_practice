b37dc557dab978c2bac9e5785fc9e97e37a92aff
Taemin <jtm990506@gmail.com>
2022-05-13 15:58:15 +0900

    [jump to django] 2 part modified

--- mysite/pybo/forms.py
+++ mysite/pybo/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Question  #pybo.models와 똑같음
+from .models import Question, Answer  #pybo.models == .models 와 똑같음
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question    # 사용할 모델
         fields = ['subject', 'content']     # QuestionForm에서 사용할 Question 속성
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content' : '답변내용',
+        }
